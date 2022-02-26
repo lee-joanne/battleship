fc23822574b5c031b7d3d3fb22eba70d39d1d551
Joanne <lee.joanne54@gmail.com>
2022-02-26 18:48:57 +0000

    Fix function to loop when user gives an invalid response on if they wish to start the game or not

--- run.py
+++ run.py
@@ -15,20 +15,23 @@ def introduce_game():
     print('')
     print(f"Welcome aboard Pirate {user_name}! There is trouble...The enemies have stolen our beloved rum and treasure! \nThey are scattered across the Black Sea. We must shoot our cannons and try to take their ships down. \nThey have cannons of their own so they will try to hit one of our ships too! \nWe are lucky to have you Pirate {user_name}. \nThe sea board contains five by five squares in x,y coordinates. \nYou must guess the coordinates to take down their ships. The first to destroy all ships wins the rum and treasure! \nUs and the enemies both have a total of 5 ships to take down. \nAre you ready?")
     print('')
-    confirmation_response = input("aye or nay? ")
-    return confirmation_response
 
-def take_user_response():
+def ask_user_ready():
     '''
-    Function to either run or close the game depending on user's response after introducing game.
+    Asks the user if they are ready to play the game or not. Loops the response if invalid response is given.
     '''
-    conf_resp = introduce_game()
-    if (conf_resp == 'aye'):
-        print("You chose aye!")
-    elif (conf_resp == 'nay'):
-        print('you chose nay!')
-    else:
-        print("You must type either 'aye' or 'nay'!")
+    while True:
+        confirmation_response = input("aye or nay? ")
+
+        if (confirmation_response == 'aye'):
+            print("You chose aye!")
+            break
+        elif (confirmation_response == 'nay'):
+            print('you chose nay!')
+            break
+        else:
+            print("You must type either 'aye' or 'nay'!")
+
     
 def create_boards():
     '''
@@ -42,18 +45,15 @@ class GameBoard:
     def __init__(self, size, name, type):
         ## properties
         self.size = self.size
-        self.board[['.' for x in range(size)] for y in range(size)]
+        ##self.board[['.' for x in range(size)] for y in range(size)]
         self.num_ships = 5
         self.name = name
         self.type = type
         self.guesses = []
         self.ships = []
 
-    
-
-    
-
 def main():
-    take_user_response()
+    introduce_game()
+    ask_user_ready()
 
 main()
