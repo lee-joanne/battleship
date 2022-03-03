eecfa361d294dda19751802f29a25e099d3d1a46
Joanne <lee.joanne54@gmail.com>
2022-03-03 18:47:52 +0000

    Revert functions back to previous git commit history such as randomizing ship coordinates, convering row to number, etc) .

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ def introduce_game():
     '''
     Initial message to introduce title of the game.
     Input will appear for user to type in their name.
-    Paragraph will appear to explain rules of the game and ask user if they wish to continue.
+    Paragraph will appear to explain rules of the game
+    and ask user if they wish to continue.
     '''
     print('-' * 80)
     print('')
@@ -14,12 +15,23 @@ def introduce_game():
     print('-' * 80)
     user_name = input("Ahoy matey! Please enter your pirate name: ")
     print('')
-    print(f"Welcome aboard Pirate {user_name}! There is trouble...The enemies have stolen our beloved rum and treasure! \nThey are scattered across the Black Sea. We must shoot our cannons and try to take their ships down. \nThey have cannons of their own so they will try to hit one of our ships too! \nWe are lucky to have you Pirate {user_name}. \nThe sea board contains five by five squares in x,y coordinates. \nYou must guess the coordinates to take down their ships. The first to destroy all ships wins the rum and treasure! \nUs and the enemies both have a total of 5 ships to take down. \nAre you ready?")
+    print(f"Welcome aboard Pirate {user_name}! There is trouble...\
+    The enemies have stolen our beloved rum and treasure! \
+    \nThey are scattered across the Black Sea. We must shoot our \
+    cannons and try to take their ships down. \nThey have cannons \
+    of their own so they will try to hit one of our ships too! \
+    \nWe are lucky to have you Pirate {user_name}. \nThe sea board \
+    contains five by five squares in x,y coordinates. \nYou must \
+    guess the coordinates to take down their ships. The first to \
+    destroy all ships wins the rum and treasure! \nUs and the \
+    enemies both have a total of 5 ships to take down.\
+    \nAre you ready?")
     print('')
 
 def ask_user_ready():
     '''
-    Asks the user if they are ready to play the game or not. Loops the response if invalid response is given.
+    Asks the user if they are ready to play the game or not. 
+    Loops the response if invalid response is given.
     '''
     while True:
         confirmation_response = input("aye or nay? ")
@@ -39,11 +51,10 @@ class GameBoard:
     '''
     def __init__(self, name):
         self.name = name
-        self.num_ships = 5
+        self.num_ships = 3
         self.guesses = []
-        self.user_board = []
         self.coordinates = []
-        self.computer_board = [
+        self.board = [
             [" ",  " A", "  B", "  C", "  D", "  E"],
             ["1", "| |", "| |", "| |", "| |", "| |"],
             ["2", "| |", "| |", "| |", "| |", "| |"],
@@ -57,30 +68,16 @@ class GameBoard:
         Will display the board to the user.
         '''
         print("The board")
-        for row in board:
+        for row in self.board:
             joint_row = "  ".join(row)
             print(f"{joint_row}\n")
 
-    def convert_row_to_number(self):
+    def convert_row_to_number(self, rows):
         '''
         Converts the string rows into integers.
         '''
         converted_row = {"A": 0, "B": 1, "C": 2, "D": 3, "E": 4}
         return converted_row
-
-    def create_random_user_board(self):
-        """
-        needs to be done
-        """
-        self.user_board = [
-            [" ",  " A", "  B", "  C", "  D", "  E"],
-            ["1", "| |", "| |", "| |", "| |", "| |"],
-            ["2", "| |", "| |", "| |", "| |", "| |"],
-            ["3", "| |", "| |", "| |", "| |", "| |"],
-            ["4", "| |", "| |", "| |", "| |", "| |"],
-            ["5", "| |", "| |", "| |", "| |", "| |"],
-        ]
-        return self.randomize_ship_coordinates()
 
     def randomize_ship_coordinates(self):
         '''
@@ -88,25 +85,27 @@ class GameBoard:
         '''
         board_rows = ["A", "B", "C", "D", "E"]
         board_columns = [1, 2, 3, 4, 5]
-
-        current_ships_deployed: int = 0
-        while current_ships_deployed < 5:
+        coordinates = []
+        x = 0
+        while x < 5:
             rows = random.choice(board_rows)
             columns = random.choice(board_columns)
             converted_row = self.convert_row_to_number(rows)
             random_coordinates = [columns, converted_row]
-            if random_coordinates not in self.coordinates:
-                self.coordinates.append(random_coordinates)
-                self.user_board[converted_row][columns] = "|X|"
-                current_ships_deployed += 1
+            if random_coordinates in coordinates:
+                pass
+            else:
+                coordinates.append(random_coordinates)
+                x += 1
+        return coordinates
 
 def start_game():
-    game_board = GameBoard(name="user")
-    game_board.create_random_user_board()
+    pass
 
 def end_game():
     '''
-    When user chooses to end game, will create a goodbye message and give instructions to let the user know how to reactive the game.
+    When user chooses to end game, will create a goodbye message 
+    and give instructions to let the user know how to reactive the game.
     '''
     print("Goodbye! To play again, please click 'Run Program'!")
 
